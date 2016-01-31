0b7f140b81e49c659a40024d3bdbcde91ebdd528
ryosuke97 <ryosuke.y.97@gmail.com>
2016-01-31 15:07:17 +0900

    jayrlo

--- js/deviceMotion.js
+++ js/deviceMotion.js
@@ -1,23 +1,42 @@
 $(function(){
 
   // 加速度のイベント
-  window.addEventListener('devicemotion', shakeDevice);
+  // window.addEventListener('devicemotion', shakeDevice);
 
 
-  function shakeDevice(evt){
-    var x = evt.acceleration.x;
-    var y = evt.acceleration.y;
-    var z = evt.acceleration.z;
+  // function shakeDevice(evt){
+  //   var x = evt.acceleration.x;
+  //   var y = evt.acceleration.y;
+  //   var z = evt.acceleration.z;
 
-    $('#device li').eq(0).text('加速度X: ' + x);
-    $('#device li').eq(1).text('加速度Y: ' + y);
-    $('#device li').eq(2).text('加速度Z: ' + z);
+  //   $('#device li').eq(0).text('加速度X: ' + x);
+  //   $('#device li').eq(1).text('加速度Y: ' + y);
+  //   $('#device li').eq(2).text('加速度Z: ' + z);
 
-    if(Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15){
-      $('#result').text('振ったよ');
-    }
+  //   if(Math.abs(x) > 15 || Math.abs(y) > 15 || Math.abs(z) > 15){
+  //     $('#result').text('振ったよ');
+  //   }
 
 
+  // }
+
+
+
+  //傾き ジャイロセンサー
+  window.addEventListener('deviceorientation', roteDevice);
+
+  function roteDevice(evt){
+    //x軸
+    var x = evt.beta;
+    //y軸
+    var y = evt.gamma;
+    //z軸
+    var z = evt.alpha;
+
+    $('#device li').eq(0).text('傾きX: ' + x);
+    $('#device li').eq(1).text('傾きY: ' + y);
+    $('#device li').eq(2).text('傾きZ: ' + z);
   }
+
 
 });
