5734a21653b7fbf41e613d96777dc5dacbcf8e00
ryosuke97 <ryosuke.y.97@gmail.com>
2016-01-31 15:38:28 +0900

    geo

--- js/touch.js
+++ js/touch.js
@@ -1,5 +1,28 @@
 $(function(){
 console.log('DOMok!');
+
+//緯度経度
+var lon;
+var lat;
+
+//GPS
+navigator.geolocation.getCurrentPosition(function(evt){
+
+  //緯度
+  var lon = evt.coords.longitude;
+  //経度
+  var lat = evt.coods.latitude;
+
+},
+function(error){
+  console.log('GPS取れません');
+
+});
+
+
+
+
+
 // タッチイベント
   $('#ball').on('touchstart', function(){
     console.log('タッチ');
@@ -12,7 +35,12 @@ console.log('DOMok!');
 
   $('#ball').on('touchend', function(){
     $('#touchResult li').eq(2).text('touchendしたよ');
+
+    $('#geo li').eq(0).text('緯度: ' + lon);
+    $('#geo li').eq(1).text('経度: ' + lat);
   });
+
+
 
 
 
