914a77e9b64182689c2213db6a62d81fc9c80877
ryosuke97 <ryosuke.y.97@gmail.com>
2016-01-31 14:31:24 +0900

    devicemotionHTML

---
+++ js/deviceMotion.js
@@ -0,0 +1,17 @@
+$(function(){
+
+  // 加速度のイベント
+  window.addEventListener('devicemotion', shakeDevice);
+
+
+  function shakeDevice(evt){
+    var x = evt.acceleration.x;
+    var y = evt.acceleration.y;
+    var z = evt.acceleration.z;
+
+    $('#device li').eq(0).text('加速度X: ' + x);
+    $('#device li').eq(1).text('加速度Y: ' + y);
+    $('#device li').eq(2).text('加速度Z: ' + z);
+  }
+
+});
